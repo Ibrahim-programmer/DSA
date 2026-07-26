44a882ffcebcfd90545059a4f6a47461cd9ed9f6
Ibrahim-programmer <sk.sammer2006@gmail.com>
2026-07-26 19:44:34 +0530

    Time: 1 ms (41.36%) | Memory: 61.2 MB (55.96%) - LeetSync

---
+++ 189-rotate-array/rotate-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length - 1;
+        k = k % (n + 1);
+        rotateBoundaries(nums, 0, n);
+        rotateBoundaries(nums, 0, k - 1);
+        rotateBoundaries(nums, k, n);
+    }
+
+    public void rotateBoundaries(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
