b10daf68ff1729798e3e3c48a463a105ad192c42
Ibrahim-programmer <sk.sammer2006@gmail.com>
2026-09-24 12:03:36 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (57.23%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,18 +1,20 @@
 class Solution {
+    public void listSubSets(int []arr,int i,List<List<Integer>> ans,List<Integer> temp){
+        if(i==arr.length){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        listSubSets(arr,i+1,ans,temp);
+        temp.add(arr[i]);
+        listSubSets(arr,i+1,ans,temp);
+        temp.remove(temp.size() - 1);
+    }
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        int n = nums.length;
-        int tSubsets = 1<<n;
-        for(int num = 0;num<tSubsets;num++){
-            List<Integer> temp = new ArrayList<>();
-            for(int i=0;i<n;i++){
-                if((num & (1<<i))!=0){
-                    temp.add(nums[i]);
-                }
-            }
-            ans.add(temp);
-        }
+        listSubSets(nums,0,ans,new ArrayList());
         return ans;
     }
+
+
 
 }
